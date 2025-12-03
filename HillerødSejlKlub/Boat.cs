aaf1a30cb55b9a9287029f7909e6ce91af3fd71a
Ida Emilie Balch Hansen <idh004@edu.zealand.dk>
2025-12-03 12:26:16 +0100

    Har lavet 2 forskellige constructors i Boat class, tilføjet Engine class, rykket 'motoroplysninger' property fra Boat til Engine.

--- HillerødSejlKlub/Boat.cs
+++ HillerødSejlKlub/Boat.cs
@@ -8,15 +8,37 @@ namespace HillerødSejlKlub
 {
     class Boat
     {
-        public string type { get; set; }
+
+        public Engine Engine { get; set; } 
+        public string Type { get; set; }
         public string Model { get; set; }
         public string Name { get; set; }
         public string SailNumber { get; set; }
-        public string MotorOplysninger { get; set; }
-
-
         public string Measurements { get; set; }
         public int BuiltYear { get; set; }
+
+        public Boat(Engine engine, string type, string model, string name, string sailNumber, string measurements, int builtYear)
+        {
+            Engine = engine;
+            Type = type;
+            Model = model;
+            Name = name;
+            SailNumber = sailNumber;
+            Measurements = measurements;
+            BuiltYear = builtYear;
+        }
+
+        public Boat(string type, string model, string name, string sailNumber, string measurements, int builtYear)
+        {
+            Type = type;
+            Model = model;
+            Name = name;
+            SailNumber = sailNumber;
+            Measurements = measurements;
+            BuiltYear = builtYear; 
+
+        }
+
         public string VedligeholdelsesLog()
         {
             return "Vedligeholdelseslog for båden ";
