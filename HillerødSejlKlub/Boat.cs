b9cd0c59e9023afc1739cdcf196873d2cb71e8b2
Yol2625 <yol001@edu.zealand.dk>
2025-12-03 10:40:02 +0100

    Boat Class

---
+++ HillerødSejlKlub/Boat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HillerødSejlKlub
+{
+    class Boat
+    {
+        public string type { get; set; }
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public string SailNumber { get; set; }
+        public string MotorOplysninger { get; set; }
+
+
+        public string Measurements { get; set; }
+        public int BuiltYear { get; set; }
+        public string VedligeholdelsesLog()
+        {
+            return "Vedligeholdelseslog for båden ";
+        }
+        public string Skadesrapport()
+        {
+            return "Skadesrapport for båden ";
+        }
+    }
+}
