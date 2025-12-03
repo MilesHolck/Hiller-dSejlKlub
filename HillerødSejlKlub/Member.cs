3a0ec7bdc79858ddb5fbbb4e1cedf0187fb1f723
oliverpollo <olp003@edu.zealand.com>
2025-12-03 12:50:26 +0100

    CRUD fra member til MemberRepo

--- HillerødSejlKlub/Member.cs
+++ HillerødSejlKlub/Member.cs
@@ -22,47 +22,6 @@ namespace HillerødSejlKlub
             Email = email;
         }
 
-        private List<Member> members = new List<Member>();
-
-        public void AddMember(Member member)
-        {
-            members.Add(member);
-        }
-
-        public List<Member> GetAll()
-        {
-            return members;
-        }
-
-        public void RemoveMember(Member member)
-        {
-            members.Remove(member);
-        }
-
-        public Member GetMemberByName(string name) 
-        {
-            if (name != null) 
-            { 
-            return members.Find(m => m.Name == name);
-            }
-            else
-            {
-                return null;
-            }
-        }
-        public Member GetMemberById(int id) 
-        {
-            return members.Find(m => m.MemberId == id.ToString());
-        }
-        public void UpdateMember(Member member, string name, int age, int phoneNumber, string email) 
-        {
-            member.Name = name;
-            member.Age = age;
-            member.PhoneNumber = phoneNumber;
-            member.Email = email;
-        }      
-
-
     } 
 
 
