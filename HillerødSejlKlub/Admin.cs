3efcc5147270a37e0498b50db3d21798ae4ff440
camil <camilla-holck@hotmail.com>
2025-12-05 09:58:19 +0100

    rykket repo ind i mappe

--- HillerødSejlKlub/Admin.cs
+++ HillerødSejlKlub/Admin.cs
@@ -1,4 +1,5 @@
 ﻿using HillerødSejlKlub.HillerødSejlKlub;
+using HillerødSejlKlub.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- HillerødSejlKlub/Repositories/MemberRepository.cs
+++ HillerødSejlKlub/Repositories/MemberRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HillerødSejlKlub
+namespace HillerødSejlKlub.Repositories
 {
     public class MemberRepository : IUserRepository
     {
