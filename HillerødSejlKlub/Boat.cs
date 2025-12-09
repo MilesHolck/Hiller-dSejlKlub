6a8743507af4dc4ee9aeca985e9f203704f9ac4d
camil <camilla-holck@hotmail.com>
2025-12-09 09:39:25 +0100

    refactoring af  af booking

--- HillerødSejlKlub/Boat.cs
+++ HillerødSejlKlub/Boat.cs
@@ -13,16 +13,12 @@ namespace HillerødSejlKlub
         public Engine Engine { get; set; }
         public string Type { get; set; }
         public string Model { get; set; }
-        public string Name { get; set; }
+        public string BoatName { get; set; }
         public string SailNumber { get; set; }
         public string Measurements { get; set; }
         public int BuiltYear { get; set; }
         public bool IsAvailable { get; set; }
-        public DateTime BookedTimeAndDate { get; set; } //proptery for at tjekke bookinger
-
-        //dictionary til bookinger tjekker om der er en liste med actuelle bookinger.
-        public Dictionary<string, Booking?> BookingsDictionary { get; }
-        //_______________________________________________________________
+        
 
         // Damagereport liste
         public List<DamageRaport> DamageReports { get; set; } = new List<DamageRaport>();
@@ -32,22 +28,22 @@ namespace HillerødSejlKlub
             Engine = engine;
             Type = type;
             Model = model;
-            Name = name;
+            BoatName = name;
             SailNumber = sailNumber;
             Measurements = measurements;
             BuiltYear = builtYear;
-            BookingsDictionary = new Dictionary<string, Booking?>();
+            
         }
 
         public Boat(string type, string model, string name, string sailNumber, string measurements, int builtYear)
         {
             Type = type;
             Model = model;
-            Name = name;
+            BoatName = name;
             SailNumber = sailNumber;
             Measurements = measurements;
             BuiltYear = builtYear;
-            BookingsDictionary = new Dictionary<string, Booking?>();
+            
         }
 
         public void AddDamage(string description, string reportedBy)
@@ -65,62 +61,10 @@ namespace HillerødSejlKlub
             return "Skadesrapport for båden ";
         }
 
-        //booking metode C.R.U.D:_______________________________________________________
-
-        public void AddBooking(Booking booking)
-        {
-            string dateKey = booking.DateTime.ToString("yyyy-MM-dd");
-            Booking? bookingForTheDay = BookingsDictionary.GetValueOrDefault(dateKey);
-            if (bookingForTheDay == null)
-            {
-                BookingsDictionary[dateKey] = booking;
-                Console.WriteLine("Added booking: " + booking.ToString());
-            }
-            else
-            {
-                throw new Exception("Båden er allerede booket på denne dato.");
-            }
-
-        }
-
-        public void DeleteBooking(Booking bookingDelete)
-        {
-            string dateKey = bookingDelete.DateTime.ToString("yyyy-MM-dd");
-
-            if (BookingsDictionary.ContainsKey(dateKey))
-            {
-                BookingsDictionary.Remove(dateKey);
-                Console.WriteLine("Deleted booking: " + bookingDelete.ToString());
-            }
-            else
-            {
-                Console.WriteLine("No booking found for this date: " + dateKey);
-            }
-        }
-
-        public void UpdateBooking(Booking updatedBooking)
-        {
-            string dateKey = updatedBooking.DateTime.ToString("yyyy-MM-dd");
-
-            if (BookingsDictionary.ContainsKey(dateKey))
-            {
-                BookingsDictionary[dateKey] = updatedBooking;
-                Console.WriteLine("Updated booking for date nad time: " + dateKey);
-            }
-            else
-            {
-                Console.WriteLine("No existing booking found for this date: " + dateKey);
-            }
-        }
-
-
-        //metode slut____________________________________________________________
-
-
-
+        
         public override string ToString()
         {
-            return $"\nBoat: {Name} \nType: {Type} \nModel: {Model} \nSail Number: {SailNumber} \nMeasurements: {Measurements} \nBuilt Year: {BuiltYear} \nIs Available: {IsAvailable} \nEngine: {Engine}";
+            return $"\nBoat: {BoatName} \nType: {Type} \nModel: {Model} \nSail Number: {SailNumber} \nMeasurements: {Measurements} \nBuilt Year: {BuiltYear} \nIs Available: {IsAvailable} \nEngine: {Engine}";
         }
     }
 }
